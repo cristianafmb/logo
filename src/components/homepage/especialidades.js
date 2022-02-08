d02bfd205f8f7abe4eb6e87ac2bdaf703bb135ee
cristianafmb <cbaiorte@gmail.com>
2022-02-08 14:59:23 +0000

    test safari 0.2

--- src/components/homepage/especialidades.js
+++ src/components/homepage/especialidades.js
@@ -12,7 +12,7 @@ const EspecialidadesHome = ({ especialidades, location, marcacaoRef }) => (
     <div className="space-sections position-relative">
         <Row className="m-4">
             {especialidades.map((data, i) => (
-                <Col sm="12" md="3" lg="3" className="padding-r-0 padding-l-7" key={"card-especialidades-"+i}>
+                <Col sm="12" md="6" lg="6" className="padding-r-0 padding-l-7" key={"card-especialidades-"+i}>
                     <CardEspecialidades title={data.title} details={data.details} button={data.button} href={data.href} img={data.img} alt={data.alt} location={location} marcacaoRef={marcacaoRef}/>
                 </Col>
                 
