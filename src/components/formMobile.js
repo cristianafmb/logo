40625fbb6e9c5191a92531e6fbdf93fba5e22e0c
cristianafmb <cbaiorte@gmail.com>
2022-02-16 11:11:45 +0000

    trying to solve placeholder date input v1.10

--- src/components/formMobile.js
+++ src/components/formMobile.js
@@ -235,7 +235,7 @@ const FormMobile = ({ title }) => {
 
                                 </select>
 
-                                <input className="input-form gold oMedium mt-2" onfocus="(this.type='date')" onblur="if(this.value==''){this.type='text'}"
+                                <input className="input-form gold oMedium mt-2" onfocus="(this.type='date')"
                                     placeholder="dd/mm/aaaa" type="date" required id="date" name="date" />
 
                                 <select className="input-form oMedium mt-2 mb-4" type="text" required placeholder="Horário" id="hours" name="hours" defaultValue={'DEFAULT'} >
