c273fba70c3b9503ef475864dcd46b513e2955b0
cristianafmb <cbaiorte@gmail.com>
2022-02-22 15:12:32 +0000

    changes

--- src/pages/sobrenos.js
+++ src/pages/sobrenos.js
@@ -26,6 +26,7 @@ import FooterMobile from '../components/footerMobile'
 
 import Image from '../components/Images'
 
+
 const SobreNos = ({ data, location }) => {
 
   const breakpoints = useBreakpoint();
